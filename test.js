b904bdfcf426d5f2eccad717b49b4f89a56cd11a
joy-narayon-roy <joyroy261216@gmail.com>
2024-12-24 13:25:35 +0600

    all done

--- test.js
+++ test.js
@@ -6,14 +6,7 @@ const services = require("./services");
 const { Profile } = require("./models");
 const { deletePhoneById } = require("./services/phone");
 const { auth } = require("./app/firebase");
-const userdata = {
-  email: "demoyeti420@gmail.com",
-  password: "123456",
-  disabled: false,
-  displayName: "User 420",
-  photoURL:
-    "https://firebasestorage.googleapis.com/v0/b/njo-ra.appspot.com/o/logo.png?alt=media&token=d086e198-200a-4ed2-8d64-58780e8caf22",
-};
+
 async function main() {
   const conn = await db("mongodb://127.0.0.1:27017/ams");
   try {
